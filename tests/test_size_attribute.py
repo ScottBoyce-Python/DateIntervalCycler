28d5f0a3a97c853dd90957c8990501b36d453920
Scott Boyce <seboyce@usgs.gov>
2024-06-19 19:00:35 -0700

    add size and with_ tests

---
+++ tests/test_size_attribute.py
@@ -0,0 +1,137 @@
+from DateIntervalCycler import DateIntervalCycler
+import datetime as dt
+
+_month_days_29 = DateIntervalCycler.MONTH_DAYS_LEAP
+
+y0 = 2000  # Must be leap year
+yN = 2016  # Must be leap year
+
+start_list = (
+    [dt.datetime(y0, m, 1) for m in range(1, 13)]
+    + [dt.datetime(y0, m, 5) for m in range(1, 13)]
+    + [dt.datetime(y0 - 1, m, 1) for m in range(1, 13)]
+    + [dt.datetime(y0 - 1, m, 5) for m in range(1, 13)]
+    + [dt.datetime(y0 + 1, m, 1) for m in range(1, 13)]
+    + [dt.datetime(y0 + 1, m, 5) for m in range(1, 13)]
+    + [dt.datetime(y0, m, _month_days_29[m]) for m in range(1, 13)]
+    + [dt.datetime(y0 - 1, m, (0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)[m]) for m in range(1, 13)]
+    + [dt.datetime(y0 + 1, m, (0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)[m]) for m in range(1, 13)]
+    + [
+        dt.datetime(y0, 2, 28),
+        dt.datetime(y0, 2, 29),
+        dt.datetime(y0, 3, 1),
+        dt.datetime(y0 + 1, 2, 28),
+        dt.datetime(y0 + 1, 3, 1),
+    ]
+)
+
+end_list = (
+    [dt.datetime(yN, m, 1) for m in range(1, 13)]
+    + [dt.datetime(yN, m, 5) for m in range(1, 13)]
+    + [dt.datetime(yN - 1, m, 1) for m in range(1, 13)]
+    + [dt.datetime(yN - 1, m, 5) for m in range(1, 13)]
+    + [dt.datetime(yN + 1, m, 1) for m in range(1, 13)]
+    + [dt.datetime(yN + 1, m, 5) for m in range(1, 13)]
+    + [dt.datetime(yN, m, _month_days_29[m]) for m in range(1, 13)]
+    + [dt.datetime(yN - 1, m, (0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)[m]) for m in range(1, 13)]
+    + [dt.datetime(yN + 1, m, (0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)[m]) for m in range(1, 13)]
+    + [
+        dt.datetime(yN, 2, 28),
+        dt.datetime(yN, 2, 29),
+        dt.datetime(yN, 3, 1),
+        dt.datetime(yN + 1, 2, 28),
+        dt.datetime(yN + 1, 3, 1),
+    ]
+)
+
+
+cycle_list = [
+    [
+        (1, 1),  # (month, day)
+        (2, 1),
+        (3, 1),
+        (4, 1),
+        (5, 1),
+        (6, 1),
+        (7, 1),
+        (8, 1),
+        (9, 1),
+        (10, 1),
+        (11, 1),
+        (12, 1),
+    ],
+    [
+        (1, 1),
+        (3, 1),
+        (5, 1),
+        (7, 1),
+        (9, 1),
+        (11, 1),
+    ],
+    [
+        (3, 1),
+        (6, 1),
+        (9, 1),
+        (12, 1),
+    ],
+    [
+        (1, 1),
+        (1, 15),
+        (2, 1),
+        (2, 15),
+        (3, 1),
+        (4, 1),
+        (5, 1),
+        (6, 1),
+        (8, 1),
+        (9, 1),
+        (9, 5),
+        (9, 10),
+        (9, 15),
+        (9, 20),
+        (10, 1),
+        (10, 30),
+        (11, 1),
+    ],
+    [
+        (4, 20),
+    ],
+    [
+        (3, 1),
+        (9, 1),
+    ],
+    [
+        (1, 1),
+        (1, 3),
+        (1, 5),
+        (1, 7),
+        (1, 9),
+        (1, 11),
+        (1, 13),
+        (1, 15),
+        (1, 17),
+        (1, 27),
+        (2, 28),
+        (3, 10),
+        (4, 20),
+    ],
+]
+
+
+def test_size_attribute_cycle():
+    for cycles in cycle_list:
+        for start in start_list:
+            for end in end_list:
+                cid = DateIntervalCycler(cycles, start, end)
+                assert cid.size == len(cid.tolist())
+
+
+def test_size_attribute_cycle_with_set():
+    NUL = dt.datetime(9999, 1, 1)
+    for cycles in cycle_list:
+        cid = DateIntervalCycler(cycles, NUL)  # just set up object with cycles
+        for start in start_list:
+            cid.set_first_interval_start(start)
+            for end in end_list:
+                cid.set_last_interval_end(end)
+                assert len(cid) == len(cid.tolist())  # len(cid) == cid.size
---
+++ tests/test_with_constructors.py
@@ -0,0 +1,155 @@
+from DateIntervalCycler import DateIntervalCycler
+import datetime as dt
+import numpy as np
+
+
+def test_with_monthly():
+    start = dt.datetime(1900, 1, 1)
+    end = dt.datetime(2050, 12, 31)
+
+    cid = DateIntervalCycler.with_monthly(start, end)
+
+    assert len(cid) == 1812  # months
+    assert cid.size == 1812
+
+    assert (
+        cid.cycles
+        == np.array(
+            [
+                (1, 1),
+                (2, 1),
+                (3, 1),
+                (4, 1),
+                (5, 1),
+                (6, 1),
+                (7, 1),
+                (8, 1),
+                (9, 1),
+                (10, 1),
+                (11, 1),
+                (12, 1),
+            ],
+            dtype=int,
+        )
+    ).all()
+
+    cid2 = DateIntervalCycler.with_monthly(start)
+
+    assert len(cid2) == DateIntervalCycler.MAX_INTERVAL  # no end date specified
+    assert cid2.size == DateIntervalCycler.MAX_INTERVAL
+
+    assert (cid.cycles == cid2.cycles).all()
+    assert (cid.cycles == cid2.copy().cycles).all()  # generate a copy
+
+
+def test_with_monthly_end():
+    start = dt.datetime(1900, 1, 1)
+    end = dt.datetime(2050, 12, 31)
+
+    cid = DateIntervalCycler.with_monthly_end(start, end)
+
+    assert len(cid) == 1812  # months
+    assert cid.size == 1812
+
+    assert (
+        cid.cycles
+        == np.array(
+            [
+                (1, 31),
+                (2, 29),
+                (3, 31),
+                (4, 30),
+                (5, 31),
+                (6, 30),
+                (7, 31),
+                (8, 31),
+                (9, 30),
+                (10, 31),
+                (11, 30),
+                (12, 31),
+            ],
+            dtype=int,
+        )
+    ).all()
+
+    cid2 = DateIntervalCycler.with_monthly_end(start)
+
+    assert len(cid2) == DateIntervalCycler.MAX_INTERVAL  # no end date specified
+    assert cid2.size == DateIntervalCycler.MAX_INTERVAL
+
+    assert (cid.cycles == cid2.cycles).all()
+    assert (cid.cycles == cid2.copy().cycles).all()  # generate a copy
+
+
+def test_with_daily():
+    def month_day_tuples():
+        date = dt.datetime(2000, 1, 1)  # must be leap year to include (2, 29)
+        md = [(1, 1)]
+        for it in range(365):
+            date += dt.timedelta(days=1)
+            md.append((date.month, date.day))
+        return np.array(md, dtype=int)
+
+    start = dt.datetime(1900, 1, 1)
+    end = dt.datetime(2050, 12, 31)
+
+    cid = DateIntervalCycler.with_daily(start, end)
+
+    assert len(cid) == (end - start).days
+    assert cid.size == len(cid)
+
+    assert (cid.cycles == month_day_tuples()).all()
+
+    cid2 = DateIntervalCycler.with_daily(start)
+
+    assert len(cid2) == DateIntervalCycler.MAX_INTERVAL  # no end date specified
+    assert cid2.size == DateIntervalCycler.MAX_INTERVAL
+
+    assert (cid.cycles == cid2.cycles).all()
+    assert (cid.cycles == cid2.copy().cycles).all()  # generate a copy
+
+
+def test_from_year_start():
+    cycles = [
+        (1, 1),  # (month, day)
+        (4, 1),
+        (7, 1),
+        (10, 1),
+    ]
+
+    dates = [
+        dt.datetime(2000, 1, 1),
+        dt.datetime(2000, 4, 1),
+        dt.datetime(2000, 7, 1),
+        dt.datetime(2000, 10, 1),
+        dt.datetime(2001, 1, 1),
+        dt.datetime(2001, 4, 1),
+        dt.datetime(2001, 7, 1),
+        dt.datetime(2001, 10, 1),
+        dt.datetime(2002, 1, 1),
+        dt.datetime(2002, 4, 1),
+        dt.datetime(2002, 7, 1),
+        dt.datetime(2002, 10, 1),
+    ]
+
+    it = -1
+    for date in dates:
+        it += 1
+        if it == len(cycles):
+            it = 0
+        cid1 = DateIntervalCycler.from_year(cycles, date.year, starting_cycle_index=it)
+        cid2 = DateIntervalCycler(cycles, date)
+
+        assert cid1.first_interval_start == cid2.first_interval_start
+
+        assert cid1.next_get() == cid2.next_get()
+        assert cid1.next_get() == cid2.next_get()
+        assert cid1.next_get() == cid2.next_get()
+        assert cid1.next_get() == cid2.next_get()
+        assert cid1.next_get() == cid2.next_get()
+        assert cid1.next_get() == cid2.next_get()
+        assert cid1.back_get() == cid2.back_get()
+        assert cid1.back_get() == cid2.back_get()
+        assert cid1.next_get() == cid2.next_get()
+        assert cid1.next_get() == cid2.next_get()
+        assert cid1.next_get() == cid2.next_get()
