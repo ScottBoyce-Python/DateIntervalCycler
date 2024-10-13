ed187cb7d3fc5102facae073e6db6ecb8a4b5c6f
Scott Boyce <seboyce@usgs.gov>
2024-10-13 11:48:08 -0700

    fix: _metadata __credits__ field with actual names

--- DateIntervalCycler/_metadata.py
+++ DateIntervalCycler/_metadata.py
@@ -4,11 +4,11 @@ Metadata associated with DateIntervalCycler.py
 
 __version__ = "0.1.2"
 __author__ = "Scott E. Boyce"
+__credits__ = "Scott E. Boyce"
+__maintainer__ = "Scott E. Boyce"
 __email__ = "boyce@engineer.com"
 __license__ = "MIT"
 __status__ = "Development"  # set to "Prototype", "Development", "Production"
-__maintainer__ = "Scott E. Boyce"
-__credits__ = "Contributor One, Contributor Two"
 __url__ = "https://github.com/ScottBoyce-Python/DateIntervalCycler"
 __description__ = "Cycle datetime intervals from a start date and list of (month, day) tuples."
 __copyright__ = "Copyright (c) 2024 Scott E. Boyce"
